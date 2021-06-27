816d0feb2dbdce238bc1526909bccc5c20c794eb
ljsjiansong <849405380@qq.com>
2021-06-27 11:23:05 +0800

    # 1、使用类的无参构造器创建

--- src/com/ljs/spring/User.java
+++ src/com/ljs/spring/User.java
@@ -1,12 +1,14 @@
 package com.ljs.spring;
 
 public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+    public User(){}
+
     public void add(String name){
         System.out.println("add......");
-    }
-
-    public static void main(String[] args) {
-        User user = new User();
-        user.add("name");
     }
 }
